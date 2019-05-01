36b685c0024db55a3074978199f5a4ea4f6a7505
Michael König <koenigmichael2002@gmail.com>
2019-05-01 14:45:16 +0200

    Add menu item actions

--- src/controller/LayoutController.java
+++ src/controller/LayoutController.java
@@ -1,9 +1,15 @@
 package controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-public class LayoutController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LayoutController implements Initializable {
     @FXML
     public MenuBar menuBar;
     @FXML
@@ -24,4 +30,27 @@ public class LayoutController {
     public TableColumn tcolName;
     @FXML
     public TableColumn tcolSize;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initMenu();
+    }
+
+    /**
+     * Initialize the menubar
+     */
+    private void initMenu() {
+        this.menuItemClose.setOnAction(event -> Platform.exit());
+
+        this.menuItemAbout.setOnAction(event -> {
+            String url = "https://github.com/koenigscode/file-explorer";
+
+            try {
+                java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+            } catch (IOException e) {
+                System.out.println("About page could not be opened");
+            }
+        });
+    }
 }
